2889bfad04ed53679a5256168a2ba68f8e898387
bya2 <byaa1972@gmail.com>
2022-11-19 20:24:53 +0900

    Add arrOf static method

--- src/Number/Divisor.ts
+++ src/Number/Divisor.ts
@@ -8,7 +8,7 @@ export default class Divisor {
   }
 
   /**
-   * 숫자가 가지고 있는 약수 리스트를 Set으로 리턴
+   * 숫자에 대한 약수 리스트를 Set으로 리턴
    * @param _number
    */
   static setOf(_number: number): Set<number> {
@@ -16,5 +16,16 @@ export default class Divisor {
     for (let i = 1, len = _number / 2; i <= len; ++i) if (_number % i === 0) set.add(i);
     set.add(_number);
     return set;
+  }
+
+  /**
+   * 숫자에 대한 약수 리스트를 배열로 리턴
+   * @param _number
+   */
+  static arrOf(_number: number): number[] {
+    const arr = [];
+    for (let i = 1, len = _number / 2; i <= len; ++i) if (_number % i === 0) arr.push(i);
+    arr.push(_number);
+    return arr;
   }
 }
