ddff3ce5d2873b6e3563a8af7b26fd0f230087e6
bya2 <byaa1972@gmail.com>
2022-11-25 22:18:46 +0900

    Add Length0, InvalidConstant constants

--- src/misc/messages.ts
+++ src/misc/messages.ts
@@ -3,3 +3,6 @@ export const NoSuchElementEnception = "";
 
 export const WARN_NodeAlreadyExist = "";
 export const WARN_NoSuchNodeExist = "";
+
+export const Length0 = "List length 0";
+export const InvalidConstant = "Invalid constant";
