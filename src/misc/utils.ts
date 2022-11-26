dcf833d8b3e098e0cb8f7841439d43876b25f97f
bya2 <byaa1972@gmail.com>
2022-11-26 16:12:31 +0900

    Add erasing methods

--- src/misc/utils.ts
+++ src/misc/utils.ts
@@ -1,1 +1,7 @@
 export const noop = () => {};
+
+export const erasing = {
+  linear: (n: number) => n,
+  elastic: (n: number) => n * (33 * n * n * n * n - 106 * n * n * n + 126 * n * n - 67 * n + 15),
+  inExpo: (n: number) => Math.pow(2, 10 * (n - 1)),
+};
