1a662d4f4b761dd961dd3f77c5712cc127918cd3
bya2 <byaa1972@gmail.com>
2023-09-23 23:48:39 +0900

    Replace class with function

--- src/Number/Euclidean.ts
+++ src/Number/Euclidean.ts
@@ -1,36 +1,25 @@
-export default class Euclidean {
-  /**
-   * @param number1
-   * @param number2
-   * @returns 공약수
-   */
-  static gcdOf(number1: number, number2: number): number {
-    return number1 % number2 === 0 ? number2 : this.gcdOf(number2, number1 % number2);
-  }
+/**
+ * 두 수의 최대공약수
+ * @param n
+ * @param m
+ */
+export const gcdOf = (n: number, m: number): number => {
+  return n % m === 0 ? m : gcdOf(m, n % m);
+};
 
-  /**
-   * @param number1
-   * @param number2
-   * @returns 공배수
-   */
-  static lcmOf(number1: number, number2: number): number {
-    return (number1 * number2) / this.gcdOf(number1, number2);
-  }
+/**
+ * 두 수의 최소공배수
+ * @param n
+ * @param m
+ */
+export const lcmOf = (n: number, m: number): number => {
+  return (n * m) / gcdOf(n, m);
+};
 
-  /**
-   * @param numbers
-   * @returns 공약수
-   */
-  static gcdOfAll(...numbers: number[]): number {
-    return numbers.reduce((number1, number2) => this.gcdOf(number1, number2));
-  }
-
-  /**
-   * @param number1
-   * @param number2
-   * @returns 공약수, 공배수
-   */
-  static gcmAndLcmOf(number1: number, number2: number): [number, number] {
-    return [this.gcdOf(number1, number2), this.lcmOf(number1, number2)];
-  }
-}
+/**
+ * 여러 수에 대한 최대공약수
+ * @param numbers
+ */
+export const gcdFor = (...numbers: number[]): number => {
+  return numbers.reduce((n, m) => gcdOf(n, m));
+};
