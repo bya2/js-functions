11f50bc6eb8f5ce9783f971bccb706920a4b206c
bya2 <byaa1972@gmail.com>
2023-11-26 23:47:34 +0900

    Complete methods and write test templates

--- src/collections/LinkedList.ts
+++ src/collections/LinkedList.ts
@@ -1,67 +1,51 @@
 class Node<T> {
   _inner: T;
-  prev?: NonNullable<Node<T>>;
-  next?: NonNullable<Node<T>>;
+  prev?: Node<T>;
+  next?: Node<T>;
 
   constructor(item: T) {
     this._inner = item;
   }
+
+  get item(): T {
+    return this._inner;
+  }
 }
 
-interface LinkedListStruct<T> {
-  head?: NonNullable<Node<T>>;
-  tail?: NonNullable<Node<T>>;
+interface ListStruct<T> {
+  head?: Node<T>;
+  tail?: Node<T>;
   get length(): number;
 }
 
-interface ImplLinkedList<T> {
-  at(index: number): T | undefined;
+interface ImplList<T> {
+  at(index: number): T;
 
   unshift(...items: T[]): void;
-
   shift(): T | undefined;
-
   push(item: T): void;
-
   pop(): T | undefined;
-
   insert(item: T, index: number): void;
-
-  extract(index: number): T;
+  extract(index: number): T | undefined;
 
   find<S extends T>(predicate: (value: T, index: number, obj: LinkedList<T>) => value is S, thisArg?: any): T | undefined;
   // find(predicate: (value: T, index: number, obj: LinkedList<T>) => unknown, thisArg?: any): T | undefined;
-
   findIndex(predicate: (value: T, index: number, obj: LinkedList<T>) => unknown, thisArg?: any): number;
-
-  toArray(): T[];
-
   every<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): this is S[];
   every(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean;
-
   some(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean;
-
   filter(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): T[];
-
   reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T): T;
   reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T, initialValue: T): T;
   reduce<U>(callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U, initialValue: U): U;
-
   clone(): LinkedList<T>;
+  toArray(): T[];
 }
 
-export default class LinkedList<T> implements LinkedListStruct<T>, ImplLinkedList<T> {
+export default class LinkedList<T> implements ListStruct<T>, ImplList<T> {
   head?: NonNullable<Node<T>>;
   tail?: NonNullable<Node<T>>;
-  #len = 0;
-
-  constructor(item?: T) {
-    if (item) {
-      this.tail = this.head = new Node(item);
-      this.#len = 1;
-      [].find;
-    }
-  }
+  #len: number = 0;
 
   get length() {
     return this.#len;
@@ -73,8 +57,18 @@ export default class LinkedList<T> implements LinkedListStruct<T>, ImplLinkedLis
     }
   }
 
-  #at(index: number): Node<T> | undefined {
-    index = Math.max(Math.min(index, this.#len - 1), 0);
+  #validateIndex(index: number) {
+    if (index < 0 && index >= this.#len) throw new Error();
+  }
+
+  #normalizeIndex(index: number): number {
+    if (index < 0) index += this.#len;
+    this.#validateIndex(index);
+    return index;
+  }
+
+  #nodeAt(index: number): Node<T> {
+    index = this.#normalizeIndex(index);
 
     let node: Node<T>;
 
@@ -89,8 +83,8 @@ export default class LinkedList<T> implements LinkedListStruct<T>, ImplLinkedLis
     return node;
   }
 
-  at(index: number): T | undefined {
-    index = Math.max(Math.min(index, this.#len - 1), 0);
+  at(index: number): T {
+    index = this.#normalizeIndex(index);
 
     let node: Node<T>;
 
@@ -115,13 +109,12 @@ export default class LinkedList<T> implements LinkedListStruct<T>, ImplLinkedLis
       } else {
         this.tail = this.head = node;
       }
+      this.#len++;
     }
-
-    this.#len += items.length;
   }
 
   shift(): T | undefined {
-    if (!this.head) return;
+    if (!this.head) return undefined;
 
     const value = this.head._inner;
 
@@ -168,24 +161,65 @@ export default class LinkedList<T> implements LinkedListStruct<T>, ImplLinkedLis
     return value;
   }
 
+  /**
+   * 1. 인덱스 유효성 검사, 정규화
+   * 2-1. 지정 노드의 전 노드의 next에 생성 노드를 바인딩
+   * 2-2. 지정 노드의 prev에 생성 노드를 바인딩
+   * @param item
+   * @param index
+   */
   insert(item: T, index: number): void {
-    // 인덱스 유효 여부
-    // 해당 인덱스 전 노드의 next에 바인딩
-    // 해당 인덱스 노드의 prev에 바인딩
+    if (index === this.#len) {
+      return this.push(item);
+    }
 
-    if (index >= this.#len) throw new Error();
-    if (index < 0) index += this.#len;
-    if (index === 0) this.unshift(item);
+    index = this.#normalizeIndex(index);
 
-    const node = new Node(item);
+    if (index === 0) {
+      return this.unshift(item);
+    }
 
-    const curr = this.#at(index);
+    const x = new Node(item);
+
+    const next = this.#nodeAt(index);
+
+    // 인덱스에 해당되는 노드의 전 노드의 NEXT에 생성된 노드를 바인딩
+    if (next.prev) {
+      const prev = next.prev;
+      prev.next = x;
+      x.prev = prev;
+    }
+
+    // 인덱스에 해당되는 노드의 PREV에 생성된 노드를 바인딩
+    next.prev = x;
+    x.next = next;
 
     this.#len++;
   }
 
-  extract(index: number): T {
-    throw new Error("Method not implemented.");
+  extract(index: number): T | undefined {
+    index = this.#normalizeIndex(index);
+
+    if (index === 0) {
+      return this.shift();
+    }
+
+    if (index === this.#len - 1) {
+      return this.pop();
+    }
+
+    const x = this.#nodeAt(index);
+    const prev = x.prev!;
+    const next = x.next!;
+
+    prev.next = x;
+    next.prev = x;
+    x.prev = prev;
+    x.next = next;
+
+    this.#len--;
+
+    return x._inner;
   }
 
   // find(predicate: (value: T, index: number, obj: LinkedList<T>) => unknown, thisArg?: any): T | undefined;
@@ -210,23 +244,23 @@ export default class LinkedList<T> implements LinkedListStruct<T>, ImplLinkedLis
   toArray(): T[] {
     return [...this];
   }
-  every<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): this is S[];
-  every(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean;
-  every(predicate: unknown, thisArg?: unknown): boolean {
-    throw new Error("Method not implemented.");
-  }
-  some(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean {
-    throw new Error("Method not implemented.");
-  }
-  filter(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): T[] {
-    throw new Error("Method not implemented.");
-  }
-  reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T): T;
-  reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T, initialValue: T): T;
-  reduce<U>(callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U, initialValue: U): U;
-  reduce(callbackfn: unknown, initialValue?: unknown): T | U {
-    throw new Error("Method not implemented.");
-  }
+  // every<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): this is S[];
+  // every(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean;
+  // every(predicate: unknown, thisArg?: unknown): boolean {
+  //   throw new Error("Method not implemented.");
+  // }
+  // some(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean {
+  //   throw new Error("Method not implemented.");
+  // }
+  // filter(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): T[] {
+  //   throw new Error("Method not implemented.");
+  // }
+  // reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T): T;
+  // reduce(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T, initialValue: T): T;
+  // reduce<U>(callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U, initialValue: U): U;
+  // reduce(callbackfn: unknown, initialValue?: unknown): T | U {
+  //   throw new Error("Method not implemented.");
+  // }
 
   clone(): LinkedList<T> {
     const clone = new LinkedList<T>();
